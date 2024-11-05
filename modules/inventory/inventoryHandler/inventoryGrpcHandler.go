22c1d3b1343fb6b108a324b2f082979d3aec4698
thanchayawik <thanchayawik@gmail.com>
2024-11-05 16:45:55 +0700

    init inventory grpc

--- modules/inventory/inventoryHandler/inventoryGrpcHandler.go
+++ modules/inventory/inventoryHandler/inventoryGrpcHandler.go
@@ -1,13 +1,26 @@
 package inventoryHandler
 
-import "github.com/thanchayawikgithub/hello-sekai-shop/modules/inventory/inventoryService"
+import (
+	"context"
+
+	inventoryPb "github.com/thanchayawikgithub/hello-sekai-shop/modules/inventory/inventoryPb"
+	"github.com/thanchayawikgithub/hello-sekai-shop/modules/inventory/inventoryService"
+)
 
 type (
 	inventoryGrpcHandler struct {
+		inventoryPb.UnimplementedInventoryGrpcServiceServer
 		inventoryService inventoryService.InventoryService
 	}
 )
 
 func NewInventoryGrpcHandler(inventoryService inventoryService.InventoryService) *inventoryGrpcHandler {
-	return &inventoryGrpcHandler{inventoryService}
+	return &inventoryGrpcHandler{
+		UnimplementedInventoryGrpcServiceServer: inventoryPb.UnimplementedInventoryGrpcServiceServer{},
+		inventoryService:                        inventoryService,
+	}
+}
+
+func (g *inventoryGrpcHandler) IsAvailableToSell(ctx context.Context, req *inventoryPb.IsAvailableToSellReq) (*inventoryPb.IsAvailableToSellRes, error) {
+	return nil, nil
 }
