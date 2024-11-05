3026bc320fc525347bf1fc953b193525821d9b4e
thanchayawik <thanchayawik@gmail.com>
2024-11-05 16:34:16 +0700

    init item  grpc

--- modules/item/itemHandler/itemGrpcHandler.go
+++ modules/item/itemHandler/itemGrpcHandler.go
@@ -1,13 +1,26 @@
 package itemHandler
 
-import "github.com/thanchayawikgithub/hello-sekai-shop/modules/item/itemService"
+import (
+	"context"
+
+	itemPb "github.com/thanchayawikgithub/hello-sekai-shop/modules/item/itemPb"
+	"github.com/thanchayawikgithub/hello-sekai-shop/modules/item/itemService"
+)
 
 type (
 	itemGrpcHandler struct {
+		itemPb.UnimplementedItemGrpcServiceServer
 		itemService itemService.ItemService
 	}
 )
 
 func NewItemGrpcHandler(itemService itemService.ItemService) *itemGrpcHandler {
-	return &itemGrpcHandler{itemService}
+	return &itemGrpcHandler{
+		UnimplementedItemGrpcServiceServer: itemPb.UnimplementedItemGrpcServiceServer{},
+		itemService:                        itemService,
+	}
+}
+
+func (g *itemGrpcHandler) FindItemsInIds(ctx context.Context, req *itemPb.FindItemsInIdsReq) (*itemPb.FindItemsInIdsRes, error) {
+	return nil, nil
 }
