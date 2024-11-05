53945e8918c115906679ee49e0ea92830f07b301
thanchayawik <thanchayawik@gmail.com>
2024-11-05 12:21:30 +0700

    init inventory domain

--- modules/inventory/inventoryEntity.go
+++ modules/inventory/inventoryEntity.go
@@ -1,1 +1,9 @@
 package inventory
+
+type (
+	Inventory struct {
+		ID       string `json:"_id" bson:"_id,omitempty"`
+		PlayerID string `json:"player_id" bson:"player_id"`
+		ItemID   string `json:"item_id" bson:"item_id"`
+	}
+)
--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,23 @@
 package inventory
+
+import (
+	"github.com/thanchayawikgithub/hello-sekai-shop/modules/item"
+	"github.com/thanchayawikgithub/hello-sekai-shop/pkg/models"
+)
+
+type (
+	UpdateInventoryReq struct {
+		PlayerID string `json:"player_id" validate:"required,max=64"`
+		ItemID   string `json:"item_id" validate:"required,max=64"`
+	}
+
+	ItemInInventory struct {
+		InventoryID string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+
+	PlayerInventory struct {
+		PlayerID string `json:"player_id"`
+		*models.PaginateRes
+	}
+)
