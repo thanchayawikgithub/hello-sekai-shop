caf72580c2a5207c10b235af7b7d0c03aaf07f62
thanchayawik <thanchayawik@gmail.com>
2024-11-05 19:12:09 +0700

    update auth migration

---
+++ pkg/database/migration/auth.go
@@ -0,0 +1,77 @@
+package migration
+
+import (
+	"context"
+	"log"
+
+	"github.com/thanchayawikgithub/hello-sekai-shop/config"
+	"github.com/thanchayawikgithub/hello-sekai-shop/modules/auth"
+	"github.com/thanchayawikgithub/hello-sekai-shop/pkg/database"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func authDBConn(cfg *config.Config) *mongo.Database {
+	return database.Conn(&cfg.DB).Database("auth_db")
+}
+
+func AuthMigrate(ctx context.Context, cfg *config.Config) {
+	db := authDBConn(cfg)
+	defer db.Client().Disconnect(ctx)
+
+	//auth
+	col := db.Collection("auth")
+	indexs, err := col.Indexes().CreateMany(ctx, []mongo.IndexModel{
+		{Keys: bson.D{{Key: "_id", Value: 1}}},
+		{Keys: bson.D{{Key: "player_id", Value: 1}}},
+		{Keys: bson.D{{Key: "refresh_token", Value: 1}}},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, index := range indexs {
+		log.Printf("Index: %s", index)
+	}
+
+	//roles
+	col = db.Collection("roles")
+	indexs, err = col.Indexes().CreateMany(ctx, []mongo.IndexModel{
+		{Keys: bson.D{{Key: "_id", Value: 1}}},
+		{Keys: bson.D{{Key: "code", Value: 1}}},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, index := range indexs {
+		log.Printf("Index: %s", index)
+	}
+
+	documents := func() []interface{} {
+		roles := []*auth.Role{
+			{
+				Title: "user",
+				Code:  0,
+			},
+			{
+				Title: "admin",
+				Code:  1,
+			},
+		}
+
+		docs := make([]interface{}, 0)
+
+		for _, role := range roles {
+			docs = append(docs, role)
+		}
+		return docs
+	}()
+
+	results, err := col.InsertMany(ctx, documents)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Migrate auth completed: ", results)
+}
--- pkg/database/script/migration.go
+++ pkg/database/script/migration.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/thanchayawikgithub/hello-sekai-shop/config"
+	"github.com/thanchayawikgithub/hello-sekai-shop/pkg/database/migration"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Check if the state and service arguments are provided
+	if len(os.Args) < 3 {
+		log.Fatal("Error: state and service arguments are required")
+	}
+
+	state := os.Args[1]   // e.g., "dev"
+	service := os.Args[2] // e.g., "auth"
+
+	// Load configuration based on state and service
+	cfg := config.LoadConfig(state, service)
+
+	switch cfg.App.Name {
+	case "auth":
+		migration.AuthMigrate(ctx, cfg)
+	case "inventory":
+	case "item":
+	case "payment":
+	case "player":
+	}
+}
