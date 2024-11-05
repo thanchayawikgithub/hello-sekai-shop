83c9ed0feac8796652d985df68a2a177c89f36f5
thanchayawik <thanchayawik@gmail.com>
2024-11-05 12:48:53 +0700

    init auth grpc

--- modules/auth/authHandler/authGrpcHandler.go
+++ modules/auth/authHandler/authGrpcHandler.go
@@ -1,15 +1,30 @@
 package authHandler
 
 import (
+	"context"
+
+	authPb "github.com/thanchayawikgithub/hello-sekai-shop/modules/auth/authPb"
 	"github.com/thanchayawikgithub/hello-sekai-shop/modules/auth/authService"
 )
 
 type (
 	authGrpcHandler struct {
+		authPb.UnimplementedAuthGrpcServiceServer
 		authService authService.AuthService
 	}
 )
 
 func NewAuthGrpcHandler(authService authService.AuthService) *authGrpcHandler {
-	return &authGrpcHandler{authService}
+	return &authGrpcHandler{
+		UnimplementedAuthGrpcServiceServer: authPb.UnimplementedAuthGrpcServiceServer{},
+		authService:                        authService,
+	}
+}
+
+func (g *authGrpcHandler) CredentialSearch(ctx context.Context, req *authPb.CredentialSearchReq) (*authPb.CredentialSearchRes, error) {
+	return nil, nil
+}
+
+func (g *authGrpcHandler) RolesCount(ctx context.Context, req *authPb.RolesCountReq) (*authPb.RolesCountRes, error) {
+	return nil, nil
 }
