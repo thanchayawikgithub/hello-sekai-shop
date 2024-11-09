4762e8cc709442faad8ef783b42fd2b2bd190ca5
thanchayawik <thanchayawik@gmail.com>
2024-11-09 18:13:27 +0700

    update jwt method

--- pkg/jwtauth/jwtauth.go
+++ pkg/jwtauth/jwtauth.go
@@ -1,6 +1,7 @@
 package jwtauth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -75,6 +76,88 @@ func NewAccessToken(secret string, expriesAt int64, claims *Claims) AuthFactory 
 	}
 }
 
+func NewRefreshToken(secret string, expriesAt int64, claims *Claims) AuthFactory {
+	return &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "hello-sekai.com",
+					Subject:   "refresh-token",
+					Audience:  []string{"hello-sekai.com"},
+					ExpiresAt: jwtTimeDurationCal(expriesAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func ReloadToken(secret string, expriesAt int64, claims *Claims) string {
+	token := &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "hello-sekai.com",
+					Subject:   "refresh-token",
+					Audience:  []string{"hello-sekai.com"},
+					ExpiresAt: jwtTimeRepeatAdapter(expriesAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+
+	return token.SignToken()
+}
+
+func NewApiKey(secret string, claims *Claims) AuthFactory {
+	return &apiKey{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "hello-sekai.com",
+					Subject:   "api-key",
+					Audience:  []string{"hello-sekai.com"},
+					ExpiresAt: jwtTimeDurationCal(31560000),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func ParseToken(secret string, tokenString string) (*AuthMapClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &AuthMapClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("error: unexpected signing method")
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenMalformed) {
+			return nil, errors.New("error: invalid token format")
+		} else if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, errors.New("error: token expired")
+		}
+
+		return nil, errors.New("error: invalid token")
+	}
+
+	if claims, ok := token.Claims.(*AuthMapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("error: invalid token claims")
+}
 func now() time.Time {
 	loc, _ := time.LoadLocation("Asia/Bangkok")
 	return time.Now().In(loc)
